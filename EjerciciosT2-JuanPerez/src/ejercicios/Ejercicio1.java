936a8cd1c02374a72d16757f026a389689a58a23
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2022-10-18 21:45:32 +0200

    ej 1 comentarios hecho queda pruebas y diagrama

--- EjerciciosT2-JuanPerez/src/ejercicios/Ejercicio1.java
+++ EjerciciosT2-JuanPerez/src/ejercicios/Ejercicio1.java
@@ -6,46 +6,56 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 
-		int numeroIntroducido=0, unidad=numeroIntroducido%10, decena=numeroIntroducido/10,
-		 centena=numeroIntroducido/100, millar=numeroIntroducido/1000;
+		int numeroIntroducido, unidad, decena, centena, millar; //Declaro las variables
 		
-		//añadir else a los if de dentro de else if
+		//Creo el Scanner, lo nombro dogma y lo importo 
 		Scanner dogma = new Scanner(System.in);
 		
-		System.out.print("Introduzca un numero entero entre 0 y 9999");
+		//Solicito al usuario un numero por consola
+		System.out.print("Introduzca un numero entero entre 0 y 9999 ");
 		
+		//Asigno valor a variable numeroIntroducido mediante uso del Scanner
 		numeroIntroducido = dogma.nextInt();
 		
+		//Asigno valor a variable unidad con modulo 10
+		unidad=numeroIntroducido%10;
+		
+		//Divido numeroIntroducido entre 10 y luego uso modulo 10 para dar un valor a variable decena 
+		decena=numeroIntroducido/10%10;
+
+		//Divido numeroIntroducido entre 100 y luego uso modulo 10 para dar un valor a variable centena 
+		centena=numeroIntroducido/100%10;
+		
+		//Divido numeroIntroducido entre 1000 para dar valor a variable millar
+		millar=numeroIntroducido/1000;
+		
+		//Establezco que de cumplirse una de las dos condiciones siguientes se imprima el siguiente mensaje por pantalla
 		if ((numeroIntroducido<0) || (numeroIntroducido>9999)) {
 			System.out.print("Numero erroneo, introduzca un numero entre 0 y 9999");
-		} else {
+			
+		//Establezco que de cumplirse todas las condiciones establecidas a continuacion se imprima el siguiente mensaje por pantalla
+			} else if ((numeroIntroducido>=0) && (numeroIntroducido<=9)) {
+				System.out.print("El numero introducido es capicua");
+				
+		//Establezco que de cumplirse todas las condiciones establecidas a continuacion se imprima el siguiente mensaje por pantalla		
+			} else if ((numeroIntroducido>=10) && (numeroIntroducido<=99) && (unidad==decena)) {
+				System.out.print("El numero introducido es capicua");	
+				
+		//Establezco que de cumplirse todas las condiciones establecidas a continuacion se imprima el siguiente mensaje por pantalla		
+			} else if ((numeroIntroducido>=100) && (numeroIntroducido<=999) && (unidad==centena)) {
+				System.out.print("El numero introducido es capicua");
+				
+		//Establezco que de cumplirse todas las condiciones establecidas a continuacion se imprima el siguiente mensaje por pantalla		
+			} else if ((numeroIntroducido>=1000) && (numeroIntroducido<=9999)
+				&& (unidad==millar) && (decena==centena)) {
+				System.out.print("El numero introducido es capicua");
+				
+		//Establezco que de no cumplirse ninguna de las condiciones anteriores se imprima el siguiente mensaje por pantalla	
+			} else {
+				System.out.print("El numero introducido no es capicua");	
+				}
 		
-		if ((numeroIntroducido>=0) && (numeroIntroducido<=9)) {
-			System.out.print("El numero introducido es capicua");
-		} else {
-			System.out.print("El numero introducido no es capicua");
-			}
-		
-		if (((numeroIntroducido>=10)) && ((numeroIntroducido<=99)) && ((unidad==decena))) {
-			System.out.print("El numero introducido es capicua");	
-		} else {	
-			System.out.print("El numero introducido no es capicua");	
-			}
-		
-		if (((numeroIntroducido>=100)) && ((numeroIntroducido<=999)) && ((unidad==centena))) {
-			System.out.print("El numero introducido es capicua");
-		} else {
-			System.out.print("El numero introducido no es capicua");	
-			} 
-		
-		if (((numeroIntroducido>=100)) && ((numeroIntroducido<=999))
-			&& ((unidad==centena)) && ((decena==centena))) {
-			System.out.print("El numero introducido es capicua");
-		} else {
-			System.out.print("El numero introducido no es capicua");	
-		
-			}
-		}	
+		//Cierro el Scanner
 		dogma.close();
 		
 	}
